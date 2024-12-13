c4c2ef56a69a5fe32de0a7afbab3b652ec62fdf9
Jovaughn Douglas <jovaughn.douglas@gmail.com>
2024-12-13 16:12:52 -0500

    Added vehicle controller and vehicle repo

--- Car-dealership-api/src/main/java/VehicleController.java
+++ Car-dealership-api/src/main/java/VehicleController.java
@@ -1,4 +1,38 @@
+import Dao.VehicleDaoMySqlImpli;
+import com.pluralsight.dealership.Vehicle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/vehicle")
 public class VehicleController {
+    private VehicleDaoMySqlImpli vdao;
 
+    @Autowired
+    public VehicleController(VehicleDaoMySqlImpli vdao) {
+        this.vdao = vdao; // Constructor
+    }
 
+    public Vehicle addVehicle(@RequestBody Vehicle vehicle) {
+        vdao.adddVehicle(vehicle);
+        return vehicle; //Create
+
+    }
+
+    @GetMapping
+    public List<Vehicle> getAll() {
+        return vdao.findAllVehicle(); // Read
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, path = "/delete/{id}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void deleteByID(@PathVariable(name = "id") Integer vin) {
+        vdao.removeVehicle(vin); //Delete
+    }
 }
+
+
+
--- Car-dealership-api/src/main/java/com/pluralsight/dealership/VehicleRepo.java
+++ Car-dealership-api/src/main/java/com/pluralsight/dealership/VehicleRepo.java
@@ -1,5 +1,17 @@
-package com.pluralsight.dealership;
-
-public class VehicleRepo {
-    
-}
+//package com.pluralsight.dealership;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.List;
+//
+//@Repository
+//public interface VehicleRepo extends JpaRepository<Vehicle, String> {
+//    List<Vehicle> findbyMake(String make);
+//    List<Vehicle> findbyModel(String model);
+//    List<Vehicle> findbyColor(String color);
+//    List<Vehicle> findbyType(String type);
+//
+//@Query("Select v from vehicle v where"+ "(:minPrice is null or v.price >= :minPrice) AND"+
+//"(:maxPrice is null or v.price <= :maxPrice) AND"
